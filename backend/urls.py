09cfd2ae14b1edccf1b76737bc0e51421783538f
link <wtflink515@gmail.com>
2015-10-13 23:40:01 +0800

    login undone, basic ui

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
 	'',
 	url(r'^$', 'backend.views.index', name='backend_index'),
-	url(r'^index/$', 'backend.views.index', name='backend_index')
-	
+	url(r'^index/$', 'backend.views.index', name='backend_index'),
+	url(r'^account/login/$', 'backend.views.login', name='backend_login'),
 )
--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from django.contrib import auth  
+from django.http.response import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
+ 	return render_to_response('index.html',locals())
+
+def login(request):
 	context = {}
-	return render(request, 'backend/index.html', context)
+
+	 if request.user.is_authenticated(): 
+        return render(request, 'backend/index.html', context)
+   
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    
+    user = auth.authenticate(username=username, password=password)
+
+    if user is not None and user.is_active:
+        auth.login(request, user)
+        return HttpResponseRedirect('backend/index.html')
+    else:
+        return render(request, 'backend/login.html', context) 
