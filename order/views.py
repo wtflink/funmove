9673bfec39325dd90196b38d56ccda288348ad97
link <wtflink515@gmail.com>
2015-12-20 00:39:23 +0800

    email template done

--- order/views.py
+++ order/views.py
@@ -4,23 +4,26 @@ from django.shortcuts import render_to_response
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 from django.core.context_processors import csrf
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.template import Context
+from django import forms
 
 from order.forms.order_form import OrderForm
 from order.models.schedule import Schedule,timeofaday
 from order.models.order import Orders
-from django.core.mail import send_mail
-
 
 
 def order_index(request):
 	return HttpResponseRedirect('/order/when/')
 
 def order(request):
+	args = {}
 	if request.method == 'POST':
 		form = OrderForm(request.POST)
 		if form.is_valid():
+			
 			order = form.save()
-
 			#save the time that has been reserved
 			time =  timeofaday.objects.get(time = order.reservation_time)
 			start = time.id
@@ -29,11 +32,18 @@ def order(request):
 				time.id = i
 				Schedule.objects.create( reservation_date = order.reservation_date, reservation_time = time )
 
-			#mailling test
-			send_mail('ur time order', 'test', 'wtflink515@gmail.com', ['wtflink515@gmail.com'], fail_silently=False)
+			#send a mail of the order information to the user by which email address they input
+			#edit the template in the templates/order/email_template.txt to change the context of it
+			send_to = form.cleaned_data['email']
+			text = get_template('order/email_template.txt')
+			mailcont = Context({ 'order': order })
+			text_content = text.render(mailcont)
+			msg = EmailMultiAlternatives('ur order', text_content, 'whatever@gmail.com', [send_to])
+			msg.send()
 
 			return HttpResponseRedirect('/order/when')
 	else:
 		form = OrderForm()
-	return render(request, 'order/order.html',locals())
+	args['form'] = form	
+	return render(request, 'order/order.html', args)
 
