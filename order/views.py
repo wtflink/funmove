69015bf420b4ef46739e7b06a323374a8632e90c
link <wtflink515@gmail.com>
2015-12-31 20:21:23 +0800

    event_json done u can see the schedule in /order/event.json fullcalender still need modify

--- order/views.py
+++ order/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
-from django.http import Http404
+from django.http import Http404,JsonResponse,HttpResponse
 from django.http.response import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.core.context_processors import csrf
@@ -7,8 +7,10 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 from django.template import Context,RequestContext
 from django import forms
-import hashlib, datetime, random
 from django.utils import timezone
+import hashlib, datetime, random, json
+from django.core.serializers.json import DjangoJSONEncoder
+
 
 from order.forms.order_form import OrderForm
 from order.models.schedule import Schedule,timeofaday
@@ -24,23 +26,32 @@ def order(request):
 		if form.is_valid():
 			#commit=false for the confirmationkey modification
 			order = form.save(commit=False)
-
 			sendto = form.cleaned_data['email']
 			salt = hashlib.sha1(str(random.random())).hexdigest()[:5]
 			order.confirmation_key = hashlib.sha1(salt+sendto).hexdigest()
-			order.key_expires = timezone.now() + datetime.timedelta(2)
+			order.key_expires = timezone.now() + datetime.timedelta(days=2)
 			order.save()
 
 			#save the time that has been reserved
-			time =  timeofaday.objects.get(time = order.reservation_time)
-			start = time.id
-			n = order.time_needed_hr*2 + order.time_needed_min/30
-			for i in range(start, start+n, 1):
-				time.id = i
-				Schedule.objects.create( reservation_date = order.reservation_date, reservation_time = time )
+			start = datetime.datetime(
+				order.reservation_date.year,
+				order.reservation_date.month, 
+				order.reservation_date.day,
+				order.reservation_time.hour,
+				order.reservation_time.minute, 0, 0)
+			end = datetime.datetime(
+				order.reservation_date.year,
+				order.reservation_date.month, 
+				order.reservation_date.day,
+				order.reservation_time.hour  + order.time_needed_hr,
+				order.reservation_time.minute+ order.time_needed_min*30, 0, 0)
+			Schedule.objects.create( reservation_start = start, 
+									 reservation_end = end, 
+									 title = 'reserved' )	
 
 			#send a mail of the order information to the user by which email address they input
 			#edit the template in the templates/order/email_template.txt to change the context of it
+			
 			text = get_template('order/email_template.txt')
 			mailcont = Context({ 'order': order })
 			text_content = text.render(mailcont)
@@ -70,34 +81,29 @@ def confirm(request, confirmation_key):
 	return render_to_response('order/confirm.html', {'order' : order}, context_instance = RequestContext(request))
 
 def events_json(request):
-    # Get all events - Pas encore terminé
-    events = Event.objects.all()
+	# Get all events 
+	events = Schedule.objects.all()
+	# Create the fullcalendar json events list
+	json_list = []
+	for event in events:
+		event_start = event.reservation_start
+		event_end = event.reservation_end
+		if event_start.hour == 0 and event_start.minute == 0:
+			allDay = True
+		else:
+			allDay = False
+		if not event.is_cancelled:
+			json_entry ={
+				'id': event.id,
+				'title': event.title,
+				'start': event_start.strftime('%Y-%m-%dT%H:%M:%S'),
+				'end': event_end.strftime('%Y-%m-%dT%H:%M:%S'),
+				'allDay': allDay
+			}
+			json_list.append(json_entry)
 
-    # Create the fullcalendar json events list
-    event_list = []
+	if len(json_list) == 0:
+		raise Http404
+	else:
+		return HttpResponse(json.dumps(json_list), content_type='application/json')
 
-    for event in events:
-        # On récupère les dates dans le bon fuseau horaire
-        event_start = event.start.astimezone(timezone.get_default_timezone())
-        event_end = event.end.astimezone(timezone.get_default_timezone())
-
-        # On décide que si l'événement commence à minuit c'est un
-        # événement sur la journée
-        if event_start.hour == 0 and event_start.minute == 0:
-            allDay = True
-        else:
-            allDay = False
-
-        if not event.is_cancelled:
-            event_list.append({
-                    'id': event.id,
-                    'start': event_start.strftime('%Y-%m-%d %H:%M:%S'),
-                    'end': event_end.strftime('%Y-%m-%d %H:%M:%S'),
-                    'title': event.title,
-                    'allDay': allDay
-                    })
-
-    if len(event_list) == 0:
-        raise http.Http404
-    else:
-        return http.HttpResponse(json.dumps(event_list),content_type='application/json')
